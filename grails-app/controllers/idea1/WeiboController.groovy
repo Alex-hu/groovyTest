e8150a1c0666e4589a5d8e25d417164bcdc1a722
Alex.hu <huzhongyuan@gmail.com>
2014-08-02 13:46:26 +0800

    modify test

--- grails-app/controllers/idea1/WeiboController.groovy
+++ grails-app/controllers/idea1/WeiboController.groovy
@@ -3,25 +3,15 @@ package idea1
 import grails.plugins.rest.client.RestBuilder
 
 class WeiboController {
-
+    def rest = new RestBuilder()
     def index() {
-        def rest = new RestBuilder()
         def resp = rest.get("https://api.weibo.com/2/statuses/public_timeline.json?access_token=2.006ZlAUCjhKUVD52b73a1105MzjvBE"){
             accept "application/json"
         }
-//        try { // expect an exception from a 404 response:
-//            twitter.head path : 'public_timeline'
-//            assert false, 'Expected exception'
-//        }
-//        catch( ex ) { assert ex.response.status == 404 }
-//
-//        assert twitter.head( path : 'home_timeline.json' ).status == 200
-
-        render "Hello my grails world huzhongyuan!" + resp.json
+        return [mesgList: resp.json.statuses];
     }
 
     def showTop() {
-        def rest = new RestBuilder()
         def resp = rest.get("https://api.weibo.com/2/statuses/public_timeline.json?access_token=2.006ZlAUCjhKUVD52b73a1105MzjvBE"){
             accept "application/json"
         }
